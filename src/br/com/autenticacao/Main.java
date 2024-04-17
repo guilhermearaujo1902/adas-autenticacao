455e4e86112130ecf4f3a495e2e344310a2b1d62
Guilherme Araujo Silva <guilherme.silva@PROWAY.TREINA>
2024-04-16 21:54:04 -0300

    :sparkles: feat(usuario): CRUD Usuario

--- src/br/com/autenticacao/Main.java
+++ src/br/com/autenticacao/Main.java
@@ -1,9 +1,21 @@
 package br.com.autenticacao;
 
+import br.com.autenticacao.controller.UsuarioController;
+import br.com.autenticacao.model.Usuario;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		UsuarioController controller = new UsuarioController();
+		
+		Usuario usuario = new Usuario();
+		usuario.setNome("Harry");
+		usuario.setEmail("harry@proway.com");
+		usuario.setSenha("Harry123");
+		usuario.setIsAtivo(true);
+		
+		controller.cadastrar(usuario);
 
 	}
 
